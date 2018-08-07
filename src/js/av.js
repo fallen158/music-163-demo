d18ef25c5acd65719e13ae55ae1c18e031e35d5f
fallen158 <79334424@qq.com>
2018-08-07 10:45:16 +0800

    初始化

---
+++ src/js/av.js
@@ -0,0 +1,16 @@
+var APP_ID = 'IpKBsGe91sspVOWCPXjdqQBx-gzGzoHsz';
+var APP_KEY = '5SH6hPpL9OtJlcHMe2hu0JLX';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+ 
+
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//   words: 'Hello World!'
+// }).then(function(object) {
+//   alert('LeanCloud Rocks!');
+// })
